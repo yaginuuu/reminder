bb3808a06484f92a31de4eba12c4c67fc42600d7
yaginuma.takuya <yaginuma.takuya@hm-p0369-no-MacBook-Air.local>
2016-03-16 15:25:45 +0900

    クラス名とメソッド名を変更しました.

--- FormatTextToRemindForChatWork.php
+++ FormatTextToRemindForChatWork.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * ChatWorkのタスクをリマインドするメッセージを整形する
+ * ChatWorkのリマインダーの通知内容を整形する
  *
  * @author yaginuuu <yaginuma.takuya@hamee.co.jp>
  * @copyright Hamee.inc All Rights Reserved
  */
-class ChatWorkTasksFormatter{
+class FormatTextToRemindForChatWork{
     const CHAT_WORK_HOST_URL = 'https://kcw.kddi.ne.jp';
     private $tasks;
 
@@ -16,12 +16,12 @@ class ChatWorkTasksFormatter{
     }
 
     /**
-     *  メッセージの整形を行う
+     *  リマインドメッセージの整形を行う
      *
      * ・期限が今日のタスクから1週間前までの期限切れのタスクを降順に並び替える
      * @return array タスク期限ごとのメッセージの配列
      */
-    public function getFormatMessage(){
+    public function getFormatMessageText(){
         $deadline_list = $this->getFormatTasks();
         $today = strtotime(date('Y/m/d'));
         $message_text = array();
@@ -65,7 +65,7 @@ class ChatWorkTasksFormatter{
     }
 
     /**
-     * 整形したChatWorkのタスク情報を取得する
+     * ChatWorkのタスク情報を整形する
      *
      * 1. タスク情報(json)を以下の条件で限定
      *  ・期限なしのタスクは排除
@@ -86,8 +86,8 @@ class ChatWorkTasksFormatter{
 
         foreach($this->tasks as $task){
             if($task['limit_time'] === 0) continue;
-            if($task['limit_time'] > $tomorrow) continue;
-            if($last_week > $task['limit_time']) continue;
+            if($tomorrow < $task['limit_time']) continue;
+            if($task['limit_time'] < $last_week) continue;
 
             $deadline_list[$task['limit_time']][] = array(
                     'task_id'                => $task['task_id'],
--- main.php
+++ main.php
@@ -3,17 +3,17 @@
 date_default_timezone_set('UTC');
 require_once('JSON.php');
 require_once('ChatWorkApi.php');
-require_once('ChatWorkTasksFormatter.php');
+require_once('FormatTextToRemindForChatWork.php');
 
 try{
     $chat_work_api = new ChatWorkApi($argv[1]);
     $chat_work_tasks = $chat_work_api->get('/v1/my/tasks?status=open');
     $myself_data = $chat_work_api->get('/v1/me');
 
-    $chat_work_tasks_fomatter = new ChatWorkTasksFormatter($chat_work_tasks);
-    $chat_work_formated_messages = $chat_work_tasks_fomatter->getFormatMessage();
+    $chat_work_tasks_fomatter = new FormatTextToRemindForChatWork($chat_work_tasks);
+    $chat_work_formatted_messages = $chat_work_tasks_fomatter->getFormatMessageText();
 
-    array_walk($chat_work_formated_messages,
+    array_walk($chat_work_formatted_messages,
             array($chat_work_api, 'sendMessage'), $myself_data['room_id']);
 }catch(Exception $e){
     echo $e->getMessage();
